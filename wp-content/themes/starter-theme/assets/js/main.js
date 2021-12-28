622d3fde7ed9e777bb8f24237acd8b639b89686a
rumesh-tharindu <rumeshtharindu96@gmail.com>
2021-12-28 16:37:56 +0530

    developed navigation menu

--- wp-content/themes/starter-theme/assets/js/main.js
+++ wp-content/themes/starter-theme/assets/js/main.js
@@ -1,3 +1,23 @@
 jQuery(document).ready(function($) {
-
+    
+    // paint api
+    (async function () {
+        if (CSS["paintWorklet"] === undefined) {
+          await import("https://unpkg.com/css-paint-polyfill");
+        }
+      
+        // The code for this worklet can be found here: https://github.com/georgedoescode/houdini-fleck-patterns/blob/main/fleck-worklet.js
+        CSS.paintWorklet.addModule("https://unpkg.com/@georgedoescode/houdini-fleck");
+      
+        // Fix a weird Safari/Firefox polyfill issue...
+        setTimeout(() => {
+          document.querySelectorAll(".site-header").forEach((el) => {
+              console.log(el)
+            el.style.width = "100%";
+          });
+        }, 250);
+      })();
+      console.log(CSS["paintWorklet"])
+    
+      // End paint api
 })
